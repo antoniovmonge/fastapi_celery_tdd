f9bbb3716eba72a1d7750456cc4423f28b5bda55
Antonio Vilchez <antoniovmonge@gmail.com>
2023-10-10 20:57:30 +0200

    [add] celery

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import os
+from celery import Celery
 from fastapi import FastAPI
 
+from dotenv import load_dotenv
+
+load_dotenv(".env/local.env")
+
 app = FastAPI()
+
+celery = Celery(
+    __name__,
+    broker=os.getenv("CELERY_BROKER_URL"),
+    backend=os.getenv("CELERY_RESULT_BACKEND"),
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@celery.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
